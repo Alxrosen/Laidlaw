0d63fbce717af985517fb2077bf8aca87ee6a922
Alxrosen <alexanderrosen3@gmail.com>
2025-06-20 16:18:02 -0400

    template done

--- analysis/religion.py
+++ analysis/religion.py
@@ -5,49 +5,76 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('/Users/alexrosen/Laidlaw/data/prison_survey.csv')
 
 '''
-v2517: Rule Violations
-
-Value Label
-1 Yes
-2 No
-7 Don't know
-8 Refused
-9 (M) Blank
+2509 -> religious group
+2517 --> rule violations
 '''
+survey_responses = ['Yes', 'No', 'Don\'t know','Refused', 'Blank']
 
-'''
-Original rule violation categories:
-v2517
-Yes           7217
-No            7024
-Blank          149
-Refused         78
-Don't know      31
-Name: count, dtype: int64
-'''
-violation_categories = ['Yes', 'No', 'Blank', 'Refused', 'Don\'t know']
-#create "dummy" variables -->  Ex:
-#                               Yes:    F, F, F, F, T, F, F ;
-#                               No :    T, T, T, T, F, F, F ;
-#                               Blank,  F, F, F, F ,F, T, T ;
-df_violation_dummies = pd.get_dummies(df['v2517'], prefix='violations')
-print( df_violation_dummies)
+# Creates data frame for "YES religion"
+yesreligion = df[df['v2509'] == 'Yes']
 
-#Adds violation dummies to df ( axis 1 means adds as new rows)
-df = pd.concat([df, df_violation_dummies], axis=1)
+# Gets the counts by each of the violation answer categories
+yr_yv = yesreligion['v2517'].value_counts().get('Yes', 0)
+yr_nv = yesreligion['v2517'].value_counts().get('No', 0)
+yr_dkv = yesreligion['v2517'].value_counts().get('Don\'t know', 0)
+yr_rv = yesreligion['v2517'].value_counts().get('Refused', 0)
+yr_bv = yesreligion['v2509'].value_counts().get('Blank', 0)
 
-### TODO Jan 12th 2025
-'''
-    Create a variable for "Yes" and "No" violations and then compare accross another factor: whether they are or are not part of a religious group
+# Calculates Percentages
+yesreligion_count = yr_yv + yr_nv + yr_dkv + yr_rv + yr_bv
+
+yr_percentages = [
+    yr_yv / yesreligion_count, 
+    yr_nv / yesreligion_count, 
+    yr_dkv / yesreligion_count, 
+    yr_rv / yesreligion_count,
+    yr_bv / yesreligion_count]
 
 
-'''
+# Plotting Religious
+plt.subplot(1,2,1)
+bars1 = plt.bar(survey_responses, yr_percentages)
+plt.ylabel("% of Total")
+plt.title("Rule Violations By Religious Group Members")
+plt.bar_label(
+    bars1,
+    labels=[f"{x:.1%}" for x in yr_percentages],  
+    padding=3, 
+    fontsize=9 
+)
 
-#print(type(violation_dummies))
-'''
-for i in df_rule_violations:
-    print(f'\n == \n')
-    counter += i.value_counts()
-    print(i.value_counts())
-print(counter)
-'''
+
+# Creates data frame for "NO religion"
+noreligion = df[df['v2509'] == 'No']
+
+# Gets the counts by each of the violation answer categories
+nr_yv = noreligion['v2517'].value_counts().get('Yes', 0)
+nr_nv = noreligion['v2517'].value_counts().get('No', 0)
+nr_dkv = noreligion['v2517'].value_counts().get('Don\'t know', 0)
+nr_rv = noreligion['v2517'].value_counts().get('Refused', 0)
+nr_bv = noreligion['v2509'].value_counts().get('Blank', 0)
+
+# Calculates Percentages
+noreligion_count = nr_yv + nr_nv + nr_dkv + nr_rv + nr_bv
+
+nr_percentages = [
+    nr_yv / noreligion_count, 
+    nr_nv / noreligion_count, 
+    nr_dkv / noreligion_count, 
+    nr_rv / noreligion_count,
+    nr_bv / noreligion_count]
+
+# Plotting Religious
+plt.subplot(1,2,2)
+bars2 = plt.bar(survey_responses, nr_percentages)
+plt.ylabel("% of Total")
+plt.title("Rule Violations By NON-Religious Group Members")
+plt.bar_label(
+    bars2,
+    labels=[f"{x:.1%}" for x in nr_percentages],  
+    padding=3, 
+    fontsize=9 
+)
+
+
+plt.show()
